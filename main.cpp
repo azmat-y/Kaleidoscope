a30e53e458d396081d9705ae44456b494e26056a
Azmat Yusuf <azmatyusuf2022@gmail.com>
2023-12-17 12:04:13 +0530

    call c functions with forward declaration

--- main.cpp
+++ main.cpp
@@ -632,6 +632,28 @@ static void MainLoop() {
   }
 }
 
+#ifdef _WIN32
+#define DLLEXPORT __declspec(dllexport)
+#else
+#define DLLEXPORT
+#endif
+
+/*
+call to c functions from our language through forward declaration
+ */
+
+/// putchard - putchar that takes a double and returns 0.
+extern "C" DLLEXPORT double putchard(double X) {
+  fputc((char)X, stderr);
+  return 0;
+}
+
+/// printd - printf that takes a double prints it as "%f\n", returning 0.
+extern "C" DLLEXPORT double printd(double X) {
+  fprintf(stderr, "%f\n", X);
+  return 0;
+}
+
 int main() {
   InitializeNativeTarget();
   InitializeNativeTargetAsmPrinter();
