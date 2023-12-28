c1c17b21eee480ca58599f6cc7f0fceb95c8a489
Azmat Yusuf <azmatyusuf2022@gmail.com>
2023-12-28 10:21:37 +0530

    making lexer recognise the conditional tokens

--- lexer.cpp
+++ lexer.cpp
@@ -38,6 +38,12 @@ int gettok() {
       return tok_def;
     if (IdentifierStr == "extern")
       return tok_extern;
+    if (IdentifierStr == "if")
+      return tok_if;
+    if (IdentifierStr == "then")
+      return tok_then;
+    if (IdentifierStr == "else")
+      return tok_else;
     return tok_identifier;
   }
 
