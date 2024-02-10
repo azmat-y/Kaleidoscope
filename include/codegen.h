d60bddaa7c652e78544ab31b4cb2e418999669d9
Azmat Yusuf <azmatyusuf2022@gmail.com>
2024-02-10 21:56:56 +0530

    made TheModule available across translation unit

--- include/codegen.h
+++ include/codegen.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "llvm/Support/Error.h"
+#include "llvm/IR/Module.h"
 #include "KaleidoscopeJIT.h"
 #include <memory>
 
@@ -10,3 +11,4 @@ void HandleTopLevelExpr();
 
 extern llvm::ExitOnError ExitOnErr;
 extern std::unique_ptr<llvm::orc::KaleidoscopeJIT> TheJIT;
+extern std::unique_ptr<llvm::Module> TheModule;
