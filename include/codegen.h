d21f2cf99ec89710c16e1e05a3642d36920201d8
Azmat Yusuf <azmatyusuf2022@gmail.com>
2024-02-21 16:02:07 +0530

    clean ups

--- include/codegen.h
+++ include/codegen.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "llvm/Support/Error.h"
 #include "llvm/IR/Module.h"
-#include "KaleidoscopeJIT.h"
 #include <memory>
 
 void InitializeModuleAndManagers();
@@ -10,5 +9,5 @@ void HandleExtern();
 void HandleTopLevelExpr();
 
 extern llvm::ExitOnError ExitOnErr;
-extern std::unique_ptr<llvm::orc::KaleidoscopeJIT> TheJIT;
+/* extern std::unique_ptr<llvm::orc::KaleidoscopeJIT> TheJIT; */
 extern std::unique_ptr<llvm::Module> TheModule;
